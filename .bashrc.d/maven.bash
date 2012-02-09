2018f458a42b4f7d5dde74c0ba3164eaf9463047
Tim Moore <tmoore@incrementalism.net>
2012-02-09 21:07:03 +1300

    Split ATLAS_OPTS definition onto multiple lines.

--- .bashrc.d/maven.bash
+++ .bashrc.d/maven.bash
@@ -1,3 +1,6 @@
-export ATLAS_OPTS="-Dfile.encoding=ISO-8859-1 -Djava.awt.headless=true -XX:+UseCompressedOops"
+ATLAS_OPTS+=" -Dfile.encoding=ISO-8859-1"
+ATLAS_OPTS+=" -Djava.awt.headless=true"
+ATLAS_OPTS+=" -XX:+UseCompressedOops"
+export ATLAS_OPTS
 export MAVEN_COLOR=true
 export MAVEN_OPTS="$ATLAS_OPTS"
