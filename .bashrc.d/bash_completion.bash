467d016545043e93451de382571defe84e49c93c
Tim Moore <tmoore@incrementalism.net>
2012-02-09 23:37:17 +1300

    Simplify bash_completion.bash.

--- .bashrc.d/bash_completion.bash
+++ .bashrc.d/bash_completion.bash
@@ -1,14 +1,9 @@
-if [ -n "`which brew`" ]; then
-    if [ -f `brew --prefix`/etc/bash_completion ]; then
-        . `brew --prefix`/etc/bash_completion
-    fi
-    if [ -f `brew --prefix`/Library/Contributions/brew_bash_completion.sh ]; then
-        . `brew --prefix`/Library/Contributions/brew_bash_completion.sh
-    fi
-elif [ -f /opt/local/etc/bash_completion ]; then
-    . /opt/local/etc/bash_completion
-elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
+if which -s brew; then
+    brew="$(brew --prefix)"
+    try_to_source "${brew}/etc/bash_completion"
+    try_to_source "${brew}/Library/Contributions/brew_bash_completion.sh"
 fi
+try_to_source "/opt/local/etc/bash_completion"
+try_to_source "/etc/bash_completion"
 
 source_everything_in ~/.bash_completion.d
