d8fd413f58bbdf3a1f34f3cfab37029568d88468
Tim Moore <tmoore@incrementalism.net>
2012-02-13 18:03:12 +1300

    Make minor formatting improvements.

--- .bashrc.d/hyperic-utils.bash
+++ .bashrc.d/hyperic-utils.bash
@@ -1,6 +1,8 @@
 find_hq() {
     local studio_host="${1}";
-    ssh atlassian-support-jump.managed.contegix.com curl -s "http://puppet01.contegix.com/node/env?${studio_host}" | awk '/hq_server:/ { print $2 }'
+    ssh atlassian-support-jump.managed.contegix.com \
+        curl -s "http://puppet01.contegix.com/node/env?${studio_host}" |
+        awk '/hq_server:/ { print $2 }'
 }
 
 open_hq() {
