TimMoore/dotfiles

commit c735eda80926a7924586a9b8fa4a3c5a10430ab6

Disable locale-sensitive string comparison.
This speeds up a lot of text-processing commands such as sort and grep.

.bashrc
@@ -18,6 +18,9 @@ source_everything_in() {
 
 # bash/readline/tty options
 
+# Disable locale-sensitive string comparison for performance
+export LC_ALL=C
+
 # Disable ctrl-s and ctrl-q for starting and stopping the terminal
 stty -ixon -ixoff stop undef start undef
 

commit f019e57242cb2dd69ed218777b49eda659ea7dec

Set LESSCHARSET to utf-8.
This avoids encoding errors caused by the change in c735eda.

.bashrc
@@ -20,6 +20,9 @@ source_everything_in() {
 
 # Disable locale-sensitive string comparison for performance
 export LC_ALL=C
+# Turn UTF-8 on explicitly for less, which
+# otherwise assumes latin-1 with LC_ALL=C
+export LESSCHARSET=utf-8
 
 # Disable ctrl-s and ctrl-q for starting and stopping the terminal
 stty -ixon -ixoff stop undef start undef