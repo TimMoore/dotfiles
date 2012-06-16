5e572522031cb6a153ab4caa8acd96a768585878
Tim Moore <tmoore@incrementalism.net>
2012-06-16 17:34:02 +1000

    Remove reference to MacPorts completion directory.

--- .bashrc.d/bash_completion.bash
+++ .bashrc.d/bash_completion.bash
@@ -6,7 +6,6 @@ fi
 if has pip; then
     eval "$(pip completion --bash)"
 fi
-try_to_source "/opt/local/etc/bash_completion"
 try_to_source "/etc/bash_completion"
 
 source_everything_in ~/.bash_completion.d
