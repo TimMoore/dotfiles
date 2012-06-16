050582e8962a809855adad0e3f97f24c21f2f7c5
Tim Moore <tmoore@incrementalism.net>
2012-06-16 17:35:51 +1000

    Remove the 'trash' function.

--- .bashrc.d/filefunctions.bash
+++ .bashrc.d/filefunctions.bash
@@ -1,16 +1,3 @@
-# Define a "trash" function on systems with a ~/.Trash directory.
-# This just moves the named files (or directories) to the trash.
-#
-if [ -d ~/.Trash ]; then
-    trash() {
-        if [ ${#} -lt 1 ]; then
-            echo 1>&2 "usage: trash file ..."
-            return 64 # 64 is the EX_USAGE exit code on BSD
-        fi
-        mv "${@}" ~/.Trash
-    }
-fi
-
 # Convenient aliases for jumping up the directory tree
 #
 alias ..='cd ..;'
