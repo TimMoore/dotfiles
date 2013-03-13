bc0d4822934743f94f416caa597e91bdbf7bfb84
Tim Moore <tmoore@incrementalism.net>
2013-03-13 16:34:39 +1100

    Remove GITHUB_TOKEN environment variable config.
    GitHub no longer supports the old API token.
    The hub command now manages its own OAuth token automatically.

--- .bashrc.d/github.bash
+++
@@ -1,5 +0,0 @@
-GITHUB_TOKEN_FILE="${HOME}/.github.token"
-if [[ -f "${GITHUB_TOKEN_FILE}" && -r "${GITHUB_TOKEN_FILE}" ]]; then
-    read GITHUB_TOKEN < "${GITHUB_TOKEN_FILE}"
-    export GITHUB_TOKEN
-fi
