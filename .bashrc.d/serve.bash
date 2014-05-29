49b9710441c82f079d0226b9fbd009360b985b2f
Tim Moore <tmoore@incrementalism.net>
2014-05-30 09:07:25 +1000

    Use Ruby for the HTTP server function.

--- .bashrc.d/serve.bash
+++ .bashrc.d/serve.bash
@@ -1,11 +1,11 @@
 # Quickly starts a webserver from the current directory.
 #
 # Thanks to:
-# https://github.com/fnichol/bashrc/blob/master/bashrc
-# http://superuser.com/questions/52483/terminal-tips-and-tricks-for-mac-os-x
+# @textgoeshere
+# https://gist.github.com/willurd/5720255
 #
 # @param [optional, Integer] bind port number, default 8000
 #
 serve() {
-    python -m SimpleHTTPServer ${1:-8000}
+    ruby -run -ehttpd . -p${1:-8000}
 }
