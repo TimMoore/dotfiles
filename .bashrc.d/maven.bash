6820924af43c425703194c5cf3ed05f6724b66b0
Tim Moore <tmoore@incrementalism.net>
2014-09-25 22:18:37 +1000

    Remove some things I don't use anymore.

--- .bashrc.d/maven.bash
+++
@@ -1,6 +0,0 @@
-ATLAS_OPTS+=" -Dfile.encoding=ISO-8859-1"
-ATLAS_OPTS+=" -Djava.awt.headless=true"
-ATLAS_OPTS+=" -XX:+UseCompressedOops"
-export ATLAS_OPTS
-export MAVEN_COLOR=true
-export MAVEN_OPTS="${ATLAS_OPTS}"
