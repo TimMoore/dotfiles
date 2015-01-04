6031f9b7737a85019599c5f7a7ea00c26a1d1fed
Tim Moore <tmoore@incrementalism.net>
2015-01-05 10:35:36 +1100

    Remove pip completion.
    I don't use it and it's very slow.

--- .bashrc.d/bash_completion.bash
+++ .bashrc.d/bash_completion.bash
@@ -3,9 +3,6 @@ if has brew; then
     try_to_source "${brew}/etc/bash_completion"
     try_to_source "${brew}/Library/Contributions/brew_bash_completion.sh"
 fi
-if has pip; then
-    eval "$(pip completion --bash)"
-fi
 try_to_source "/etc/bash_completion"
 
 source_everything_in ~/.bash_completion.d
