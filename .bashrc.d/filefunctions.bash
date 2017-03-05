c0386ee99b30e476af0361cbbd9fde2111537c06
Tim Moore <tim.moore@lightbend.com>
2017-03-05 12:10:24 +1100

    Remove 'emptydirs' function
    This can be accomplished with `find . -type d -empty`.

--- .bashrc.d/filefunctions.bash
+++ .bashrc.d/filefunctions.bash
@@ -3,9 +3,3 @@
 alias ..='cd ..;'
 alias ...='.. ..'
 alias ....='.. .. ..'
-
-emptydirs() {
-    find . -not '(' -name .svn -prune ')' -type d \
-        -exec sh -c '[ $(ls -1 "{}" | wc -l) -eq 0 ]' ';' \
-        -print
-}
