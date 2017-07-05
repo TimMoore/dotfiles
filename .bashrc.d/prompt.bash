b6510f6df78fa1763060ed266ac7def9a7c9210d
Tim Moore <tim.moore@lightbend.com>
2017-07-05 09:36:39 +0930

    Ensure there is a newline before each prompt

--- .bashrc.d/prompt.bash
+++ .bashrc.d/prompt.bash
@@ -6,6 +6,19 @@ GIT_PS1_SHOWUPSTREAM=auto
 GIT_PS1_DESCRIBE_STYLE=default
 GIT_PS1_SHOWCOLORHINTS=1
 
+force_newline() {
+    # Adapted from https://stackoverflow.com/q/19943482/29470
+    # CSI 6n reports the cursor position as ESC[n;mR, where n is the row
+    # and m is the column. Issue this control sequence and silently read
+    # the resulting report until reaching the "R". By setting IFS to ";"
+    # in conjunction with read's -a flag, fields are placed in an array.
+    local curpos
+    echo -en '\033[6n'
+    IFS=';' read -s -d R -a curpos
+    curpos[0]="${curpos[0]:2}"  # strip leading ESC[
+    (( curpos[1] > 1 )) && echo -e '\033[7m%\033[0m'
+}
+
 reset_window_title() {
     # Sometimes, SSHing to a remote server will take over the window/tab title.
     # This clears it back to the default.
@@ -34,6 +47,7 @@ init_prompt() {
         PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND; }${@}"
     }
 
+    add_prompt_cmd force_newline
     add_prompt_cmd reset_window_title
     add_prompt_cmd "__git_ps1 '${git_ps1_prefix}' '${git_ps1_suffix}'"
 
